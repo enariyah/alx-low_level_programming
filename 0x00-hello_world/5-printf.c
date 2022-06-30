2cd58a2b43091bec4f297d8703464316d505acbb
enariyah <hopmichael52@gmail.com>
2022-06-30 21:37:42 +0000

    got it

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,12 @@
+/* Description dj */
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description: print with printf
+ * Return:  nothing
+ */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("\n with proper grammar, but the outcome is a piece of art,");
 		return (0);
 }
