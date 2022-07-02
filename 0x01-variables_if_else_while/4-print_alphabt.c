839d2c9be25a25051a4063c85be2692d4c1c2b50
enariyah <hopmichael52@gmail.com>
2022-07-02 01:41:44 +0000

    ann

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entery here
+ * Description: without e and q
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+	for (a = 'a'; a <='z'; a++)
+	{
+		if (a != 'e' && a != 'q')
+		{
+			putchar(a);
+		}
+	}
+	putchar(10);
+	return (0);
+}
