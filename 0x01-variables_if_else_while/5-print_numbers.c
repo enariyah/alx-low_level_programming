3e70c1cad5e822a038d28bbe88b6bd327dc0e6d2
enariyah <hopmichael52@gmail.com>
2022-07-02 02:05:05 +0000

    fhdk

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entery here
+ * Description: numbers
+ * Return: zero
+ */
+int main(void)
+{
+	int h;
+
+	for (h = 0; h < 10; h++)
+	{
+		printf("%d", h);
+	printf("\n");
+	}
+       return (0);
+}
