894e93eb5feceaf15b7b111747959185a7e8bc4b
enariyah <hopmichael52@gmail.com>
2022-07-02 00:57:05 +0000

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entery is here
+ * Description: a-z
+ * Return: always return 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
