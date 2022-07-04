f077082148f46f9ff2f7aa3b1927ad570711d598
enariyah <hopmichael52@gmail.com>
2022-07-04 00:13:59 +0000

    hhs

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entery here
+ * description: single digits
+ * Return: zero
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar((a % 10) + '0');
+		if (a == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+		
+	}
+	putchar('\n');
+	return (0);
+}
