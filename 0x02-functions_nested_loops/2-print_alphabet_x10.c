a316b27a401a7da2276bf0e5741cda5ea0b40318
enariyah <hopmichael52@gmail.com>
2022-07-05 22:07:05 +0000

    slff

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,9 +3,9 @@
 /**
  * main - entery point
  *
- * Description: print lower case alphabets 10x
+ * Description: print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  *
- * Return: 0
+ * Return: 0.
  */
 void print_alphabet_x10(void)
 {
