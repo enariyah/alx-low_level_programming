bfa780948fdc76f20b1b8ec03e31004b7dee4dae
enariyah <hopmichael52@gmail.com>
2022-07-05 21:46:41 +0000

    al2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entery here alphabet 10x
+ * description: print alphabets 10x
+ * Return: zero
+ */
+void print_alphabet_x10(void)
+{
+	char a;
+	int b = 1;
+
+	while (b <= 10)
+	{
+		b++;
+	for (a = 'a'; a <= 'z'; a++)
+			_putchar(a);
+	_putchar('\n');
+	}
+}
