6b85ed9463b32f25e475fb47e33e421bd4839fd3
enariyah <hopmichael52@gmail.com>
2022-07-05 21:31:38 +0000

    dkfj

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entery
+ * description: prints lowercase alphabet
+ * Return: zero
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	
+		_putchar(a);
+	_putchar('\n');
+	
+}
