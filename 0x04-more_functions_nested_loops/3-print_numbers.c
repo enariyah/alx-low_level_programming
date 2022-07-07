283f9d24023855007b08b75a2d820d9cefee86e5
enariyah <hopmichael52@gmail.com>
2022-07-07 21:39:47 +0000

    nine

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0 to 9
+ * void - nothing returns
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+		_putchar(a);
+	_putchar('\n');
+}
