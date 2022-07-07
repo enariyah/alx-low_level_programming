fbb62d8bc7263e2e7c2700118fc9960f98146edb
enariyah <hopmichael52@gmail.com>
2022-07-07 22:55:32 +0000

    sjd

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the number
+ * void - return nothing
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (b = 0; b <= 9; b++)
+	{
+		for (a = 0; a <= 14; a++)
+
+		{
+			if (a > 9)
+			_putchar((a / 10) + '0');
+		_putchar((a % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
