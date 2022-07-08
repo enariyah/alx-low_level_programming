8d16760e81a696f7ad6aa8dc989b0b3fded7ad1f
enariyah <hopmichael52@gmail.com>
2022-07-08 00:59:37 +0000

    fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,21 +11,21 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a % 3 == 0)
+		if ((a % 3 == 0) && (a % 5 == 0))
 		{
-			printf("Fizz");
+			printf("FizzBuzz");
 		}
 		else if (a % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else if (a % 3 == 0 && a % 5 == 0)
+		else if (a % 3 == 0)
 		{
-			printf("FizzBuzz");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", a);
+			printf("%d", a);
 		}
 		if (a != 100)
 		{
