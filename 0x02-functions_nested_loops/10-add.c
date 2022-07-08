77a4b76ecbf01c4771942f00f3e9f7e873db5549
enariyah <hopmichael52@gmail.com>
2022-07-08 19:49:18 +0000

    fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * add - adds two integers
- *
+ *@a: the variable to be added
+ *@b: another variable
  * Return: return the result
  */
 int add(int a, int b)
 {
 	int res;
-	
-	res = a + b;
 
+	res = a + b;
 	return (res);
 }
