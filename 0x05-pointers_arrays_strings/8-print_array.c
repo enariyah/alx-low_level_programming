66beb51b46bc4537079ff6d6ed5e541ec9acf9f9
enariyah <hopmichael52@gmail.com>
2022-07-12 23:45:54 +0000

    arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_array - prints arranys
+ * @a: a variable
+ * @n: variable
+ * Return: none
+ */
+void print_array(int *a, int n)
+{
+	int a;
+	for (a = 0; a < n; a++)
+	{
+		if (i == 0)
+			printf("%d", a[a]);
+		else
+			printf(", %d", a[a]);
+	}
+	printf("\n");
+}
