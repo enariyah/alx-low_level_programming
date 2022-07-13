35d0b12e3bbc0adbeb3a3f5f15960121d25cc7ee
enariyah <hopmichael52@gmail.com>
2022-07-13 21:37:25 +0000

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strncpy - a function copies string
+ * @dest: destination variable
+ * @src: source variable
+ * @n: integer variable
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+	return (dest);
+}
