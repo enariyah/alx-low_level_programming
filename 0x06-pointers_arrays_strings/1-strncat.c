de70b1256429bb0e18faf0654d3015ecf718b652
enariyah <hopmichael52@gmail.com>
2022-07-13 21:27:08 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *_strncat - a function concatenates two strings
+ * @dest: destination variable
+ * @src: source variable
+ * @n: integer variable
+ * Return: destination string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
