c941bdb73a3b1d37444dabad3a01db19933496c4
enariyah <hopmichael52@gmail.com>
2022-07-14 20:14:34 +0000

    toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * string_toupper - converts string to upper case
+ *
+ * Return: non
+ */
+char *string_toupper(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (str[a] >= 'a' && str[a] <= 'z')
+			str[a] = str[a] - 32;
+	}
+	return (str);
+}
