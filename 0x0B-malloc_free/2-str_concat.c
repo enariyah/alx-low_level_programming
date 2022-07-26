b6095d119b7bb917117b07fbda6452de09c19571
enariyah <hopmichael52@gmail.com>
2022-07-26 19:51:45 +0000

    concatenate fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,10 @@ char *str_concat(char *s1, char *s2)
 	int l1 = 0, l2 = 0, i, ll = 0, j = 0;
 	char *a;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i]; i++)
 	{
 		l1++;
