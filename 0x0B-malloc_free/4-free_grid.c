49e435b8c1162d45f16338474f81265c399c9012
enariyah <hopmichael52@gmail.com>
2022-07-27 16:25:05 +0000

    free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * free_grid - a function to free allocated space
+ * @grid: - pointer to previous function
+ * @height: - the height of the array
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	for (int i = 0; i < height; i++)
+		free(grid[i]);
+	free(grig);
+}
