f3309d0bf29cd1eb2268ed2b0fee461b9d416862
enariyah <hopmichael52@gmail.com>
2022-08-01 23:10:52 +0000

    fixedd

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,6 +8,8 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
@@ -20,6 +22,4 @@ void print_dog(struct dog *d)
 		printf("Owner: (nil)\n");
 	else
 		printf("Owner: %s\n", d->owner);
-	if (d == NULL)
-		return;
 }
