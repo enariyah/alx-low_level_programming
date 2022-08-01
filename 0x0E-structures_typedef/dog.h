357f31dff67bbe29f0eff738e94f74dc102e7353
enariyah <hopmichael52@gmail.com>
2022-08-01 20:00:23 +0000

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - simple structrure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: this struct function is used to store name, age and owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
