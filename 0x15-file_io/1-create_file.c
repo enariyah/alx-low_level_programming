b0b7275364ba9440387f071526ec87cf09c1157f
enariyah <hopmichael52@gmail.com>
2022-08-23 03:11:31 +0300

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * create_file - function creates file
+ * @filename: name of the file to be created
+ * @text_content: content for the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, len = 0;
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+	while (text_content != NULL)
+		len++;
+	w = write(fd, text_content, len);
+	if (w < 0)
+		return (-1);
+	close(fd);
+	return (1);
+}
