bfb15a020b47b6bf159b0d0d078489ba9232b0fe
enariyah <hopmichael52@gmail.com>
2022-08-23 03:17:50 +0300

    hot fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,6 +9,8 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd, w, len = 0;
 
+	if (filename == NULL)
+		return (-1)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
