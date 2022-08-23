a23c1f62ab3610d470877c7d042eee5fe6709d14
enariyah <hopmichael52@gmail.com>
2022-08-23 03:26:22 +0300

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,14 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
+	if (text_content)
+	{
 	while (text_content != NULL)
 		len++;
 	w = write(fd, text_content, len);
 	if (w < 0)
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
