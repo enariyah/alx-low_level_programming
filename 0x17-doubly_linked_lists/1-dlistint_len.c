18b9adc0c877d350d9e5f75773897b84e66b8201
enariyah <hopmichael52@gmail.com>
2022-09-30 02:37:00 +0300

    fix'

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 /**
- *
+ * dlistint_len - fuction returns number of elements in a list
+ * @h: pointer to head file
+ * Return: number of elements
  */
 size_t dlistint_len(const dlistint_t *h)
 {
