301e0c5234d9f2a57c9cf13573e7ba36999ae471
enariyah <hopmichael52@gmail.com>
2023-04-07 23:07:21 +0300

    hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: number of hash table buckets to create
+ * Return: address of new hash table, or NULL if failure
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (ht->array == NULL)
+		return (NULL);
+
+	ht->size = size;
+
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+C projects on hash tables
